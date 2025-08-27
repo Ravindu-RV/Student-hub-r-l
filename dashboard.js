aacf5892c18fd3f0446cfe58ac4929672ceb5bf1
Ravindu Viduranga Bandara <ravinduviduranga5941@gmail.com>
2025-08-27 16:43:59 +0530

    Create dashboard.js

---
+++ dashboard.js
@@ -0,0 +1,19 @@
+// Load current user from localStorage
+const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+const profilePic = document.getElementById('profilePic');
+const username = document.getElementById('username');
+const logout = document.getElementById('logout');
+
+if(currentUser) {
+  profilePic.src = currentUser.profilePic;
+  username.textContent = currentUser.username;
+} else {
+  // If no user logged in, redirect to login page
+  window.location.href = "index.html";
+}
+
+// Logout
+logout.addEventListener('click', () => {
+  localStorage.removeItem('currentUser');
+  window.location.href = "index.html";
+});
